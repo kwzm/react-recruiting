0b46e2d405dff5bc97aa99ebbc5f6572db271494
kwzm <kwzm93@163.com>
2019-02-23 17:59:13 +0800

    9-3 个人中心信息展示2

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Result, List, WhiteSpace } from 'antd-mobile'
+
+const Item = List.Item
+const Brief = Item.Brief
 
 @connect(
   state => state.user
 )
 class User extends React.Component {
   render() {
+    const { user, avatar, type, company, title, desc, money } = this.props
+
     return (
-      <div>
-        <p>用户中心</p>
-      </div>
+      user ? (
+        <div>
+          <Result
+            img={<img style={{width: 50}} src={require(`../img/${avatar}.png`)} alt="" />}
+            title={user}
+            message={type === 'boss' ? company : null}
+          />
+          <List renderHeader={() => '简介'}>
+            <Item multipleLine>
+              {title}
+              {desc.split('\n').map(v => <Brief key={v}>{v}</Brief>)}
+              {money ? <Brief>{money}</Brief> : null}
+            </Item>
+          </List>
+          <WhiteSpace />
+          <List>
+            <Item>推出登录</Item>
+          </List>
+        </div>
+      ) : null
     )
   }
 }
