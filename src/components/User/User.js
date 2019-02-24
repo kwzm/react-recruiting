06b0b063266078e9e1d764de9e7a28f00f82c174
kwzm <kwzm93@163.com>
2019-02-24 15:05:16 +0800

    9-4 清除cookie登录状态

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Result, List, WhiteSpace } from 'antd-mobile'
+import browserCookie from 'browser-cookies'
+import { Result, List, WhiteSpace, Modal } from 'antd-mobile'
 
 const Item = List.Item
 const Brief = Item.Brief
@@ -9,6 +10,19 @@ const Brief = Item.Brief
   state => state.user
 )
 class User extends React.Component {
+  logout = () => {
+    console.log('xxx')
+    const alert = Modal.alert
+
+    alert('注销', '确认退出登录吗？', [
+      { text: '取消' },
+      { text: '确定', onPress: () => {
+        browserCookie.erase('userid')
+        window.location.href = window.location.href
+      }}
+    ])
+  }
+
   render() {
     const { user, avatar, type, company, title, desc, money } = this.props
 
@@ -29,7 +43,7 @@ class User extends React.Component {
           </List>
           <WhiteSpace />
           <List>
-            <Item>推出登录</Item>
+            <Item onClick={this.logout}>退出登录</Item>
           </List>
         </div>
       ) : null
