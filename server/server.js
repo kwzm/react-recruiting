3f38366bde99579e15137520c02d250118c43e6a
kwzm <kwzm93@163.com>
2019-03-02 15:39:00 +0800

    10-3 前后端实时显示消息

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const io = require('socket.io')(server)
 
 io.on('connection', function(socket) {
   console.log('user login')
+  socket.on('sendmsg', function(data){
+    console.log(data)
+    io.emit('recvmsg', data)
+  })
 })
 
 app.use(cookieParser())
