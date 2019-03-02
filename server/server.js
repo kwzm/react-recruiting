91734c12720f4b3f014775e9a7420be2a1fea289
kwzm <kwzm93@163.com>
2019-03-02 15:01:59 +0800

    10-2 socket.io前后端联通

--- server/server.js
+++ server/server.js
@@ -4,11 +4,17 @@ const cookieParser = require('cookie-parser')
 const userRouter = require('./user')
 
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on('connection', function(socket) {
+  console.log('user login')
+})
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user', userRouter)
 
-app.listen(9093, () => {
+server.listen(9093, () => {
   console.log('Node app start at port 9093')
 })
