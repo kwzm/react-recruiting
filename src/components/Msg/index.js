8c41ebe4372657d8f20c610d64d3a14098dd1343
kwzm <kwzm93@163.com>
2019-03-09 16:20:10 +0800

    11-2 聊天列表展示

--- src/components/Msg/index.js
+++ src/components/Msg/index.js
@@ -1,19 +1,51 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { List } from 'antd-mobile'
 
 @connect(
   state => state
 )
 class Msg extends React.Component {
+  getLast(arr) {
+    return arr[arr.length - 1]
+  }
+
   render() {
+    const Item = List.Item
+    const Brief = Item.Brief
+    const userId = this.props.user._id
+    const userInfo = this.props.chat.users
     const msgGroup = {}
 
     this.props.chat.chatmsg.forEach(v => {
       msgGroup[v.chatid] = msgGroup[v.chatid] || []
       msgGroup[v.chatid].push(v)
     })
-    console.log(msgGroup)
-    return ''
+    const chartList = Object.values(msgGroup)
+    return (
+      <div>
+        {chartList.map(v => {
+          const lastItem = this.getLast(v)
+          const targetId = v[0].from === userId ? v[0].to : v[0].from
+
+          if (!userInfo[targetId]) {
+            return null
+          }
+
+          return (
+            <List>
+              <Item 
+                key={lastItem._id}
+                thumb={require(`../img/${userInfo[targetId].avatar}.png`)}
+              >
+                {lastItem.content}
+                <Brief>{userInfo[targetId].name}</Brief>
+              </Item>  
+            </List>
+          )
+        })}
+      </div>
+    )
   }
 }
 
