81bde8e337805072f41f8cf4cac975c298a2d489
kwzm <kwzm93@163.com>
2019-03-24 17:31:51 +0800

    14-12 React16服务端渲染新Api

--- server/server.js
+++ server/server.js
@@ -50,45 +50,51 @@ app.use(function(req, res, next){
     applyMiddleware(thunk)
   ))
   const context = {}
-  const markUp = renderToString(
+  const desc = {
+    '/login': '一个用react搭建的招聘APP',
+    '/msg': 'React聊天消息列表',
+    '/boss': 'Boss查看牛人列表页面'
+  }
+  const markUpStream = renderToNodeStream(
     <Provider store={store}>
       <StaticRouter location={req.url} context={context}>
         <App />
       </StaticRouter>
     </Provider>
   )
-  const desc = {
-    '/login': '一个用react搭建的招聘APP',
-    '/msg': 'React聊天消息列表',
-    '/boss': 'Boss查看牛人列表页面'
-  }
-  const pageHtml = `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="theme-color" content="#000000">
-      <meta name="author" content="kwzm">
-      <meta name="keywords" content="react,ssr,招聘APP">
-      <meta name="description" content="${desc[req.url]}">
-      <title>React App</title>
-      <link rel="stylesheet" href="${staticPath['main.css']}"/>
-    </head>
-    <body>
-      <noscript>
-        You need to enable JavaScript to run this app.
-      </noscript>
-      <div id="root">${markUp}</div>
-      <script src="${staticPath['main.js']}"></script>
-    </body>
-  </html>  
-  `
 
   if (context.url) {
     res.redirect(301, context.url);
   } else {
-    res.send(pageHtml)
+    res.write(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <meta name="theme-color" content="#000000">
+          <meta name="author" content="kwzm">
+          <meta name="keywords" content="react,ssr,招聘APP">
+          <meta name="description" content="${desc[req.url]}">
+          <title>React App</title>
+          <link rel="stylesheet" href="${staticPath['main.css']}"/>
+        </head>
+        <body>
+          <noscript>
+            You need to enable JavaScript to run this app.
+          </noscript>
+          <div id="root">
+    `)
+    markUpStream.pipe(res, {end: false})
+    markUpStream.on('end', () => {
+      res.write(`
+          </div>
+          <script src="${staticPath['main.js']}"></script>
+          </body>
+        </html>  
+      `)
+      res.end()
+    })
   }
 })
 
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = createStore(reducers, composeEnhancers(
   applyMiddleware(thunk)
 ))
 
-ReactDOM.render(
+ReactDOM.hydrate(
   (
     <Provider store={store}>
       <BrowserRouter>
