967734433cba93407059a325c295b123b81ee3a3
kwzm <kwzm93@163.com>
2019-03-24 15:54:02 +0800

    14-8 服务端SSR代码改造

--- server/server.js
+++ server/server.js
@@ -10,14 +10,19 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 import React from 'react'
-import { renderToNodeStream } from 'react-dom/server'
+import { renderToNodeStream, renderToString } from 'react-dom/server'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { StaticRouter } from 'react-router-dom'
 import csshook from 'css-modules-require-hook/preset'
+require('asset-require-hook')({
+  extensions: ['png'],
+  limit: 10000
+})
 import App from '../src/App'
-import reducers from '../reducer'
+import reducers from '../src/reducer'
+import staticPath from '../build/asset-manifest.json'
 
 io.on('connection', function(socket) {
   socket.on('sendmsg', function(data){
@@ -45,18 +50,45 @@ app.use(function(req, res, next){
     applyMiddleware(thunk)
   ))
   const context = {}
-  const markUp = renderToNodeStream(
+  const markUp = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.url} context={context}>
         <App />
       </StaticRouter>
     </Provider>
   )
+  const desc = {
+    '/login': '一个用react搭建的招聘APP',
+    '/msg': 'React聊天消息列表',
+    '/boss': 'Boss查看牛人列表页面'
+  }
+  const pageHtml = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="theme-color" content="#000000">
+      <meta name="author" content="kwzm">
+      <meta name="keywords" content="react,ssr,招聘APP">
+      <meta name="description" content="${desc[req.url]}">
+      <title>React App</title>
+      <link rel="stylesheet" href="${staticPath['main.css']}"/>
+    </head>
+    <body>
+      <noscript>
+        You need to enable JavaScript to run this app.
+      </noscript>
+      <div id="root">${markUp}</div>
+      <script src="${staticPath['main.js']}"></script>
+    </body>
+  </html>  
+  `
 
   if (context.url) {
     res.redirect(301, context.url);
   } else {
-    return res.sendFile(path.resolve('build/index.html'))
+    res.send(pageHtml)
   }
 })
 
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import logoImg from './job.png'
 import './Logo.css'
 
 const Logo = () => (
   <div className="logo-container">
-    <img src={logoImg} alt="logo" />
+    <img src={require('./job.png')} alt="logo" />
   </div>
 )
 
